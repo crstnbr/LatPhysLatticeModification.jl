cf175741d4ad3f3ac0bb5dc6a562140cc3ce632f
Jan Attig <jattig@thp.Uni-Koeln.DE>
2018-11-29 13:07:28 +0100

    added removing of multiple sites

--- src/site_add_remove.jl
+++ src/site_add_remove.jl
@@ -89,3 +89,117 @@ end
 
 # export the function
 export removeSite!
+
+
+
+
+
+################################################################################
+#
+#   REMOVING MULTIPLE SITES
+#       - from unitcells
+#       - from lattices
+#
+################################################################################
+
+
+# Remove multiple sites from a unitcell (and return them)
+function removeSite!(
+        unitcell :: U,
+        index    :: Integer ...
+    ) :: Vector{S} where {D,N,LS,LB, S<:AbstractSite{LS,D}, B<:AbstractBond{LB,N}, U<:AbstractUnitcell{S,B}}
+
+    # get the list of sites
+    sites_to_return = S[site(unitcell,i) for i in index]
+
+    # get all sites of the unitcell
+    site_list = sites(unitcell)
+    # build a index list
+    indices_new = collect(1:length(site_list))
+    # fill the new index list at positions of removed sites
+    for i in index
+        indices_new[i] = -1
+    end
+    # fill the new list successively from smallest to largest
+    if indices_new[1] == -1
+        indices_new[1] = 0
+    end
+    for i in 2:length(indices_new)
+        if indices_new[i] == -1
+            indices_new[i] = indices_new[i-1]
+        else
+            indices_new[i] = indices_new[i-1] + 1
+        end
+    end
+    # get a new site list
+    site_list = S[site_list[i] for i in 1:length(site_list) if !(i in index)]
+    # register new list of sites within the unitcell object
+    sites!(unitcell, site_list)
+
+
+    # get all bonds that are not connected to any removed site
+    bond_list = filter(b -> !(to(b) in index || from(b) in index), bonds(unitcell))
+    # relabeling of bond indices
+    for b in bond_list
+        # set the new indices
+        from!(b, indices_new[from(b)])
+        to!(b,   indices_new[to(b)])
+    end
+    # set the bond list in the unitcell object
+    bonds!(unitcell, bond_list)
+
+
+    # return the removed sites
+    return sites_to_return
+end
+
+# Remove multiple sites from a lattice (and return them)
+function removeSite!(
+        lattice :: L,
+        index   :: Integer ...
+    ) :: Vector{S} where {D,N,LS,LB, S<:AbstractSite{LS,D}, B<:AbstractBond{LB,N}, U<:AbstractUnitcell{S,B},
+        DL,NL,LSL,LBL,SL<:AbstractSite{LSL,DL}, BL<:AbstractBond{LBL,NL}, L<:AbstractLattice{SL,BL,U}}
+
+    # get the list of sites
+    sites_to_return = S[site(lattice,i) for i in index]
+
+    # get all sites of the lattice
+    site_list = sites(lattice)
+    # build a index list
+    indices_new = collect(1:length(site_list))
+    # fill the new index list at positions of removed sites
+    for i in index
+        indices_new[i] = -1
+    end
+    # fill the new list successively from smallest to largest
+    if indices_new[1] == -1
+        indices_new[1] = 0
+    end
+    for i in 2:length(indices_new)
+        if indices_new[i] == -1
+            indices_new[i] = indices_new[i-1]
+        else
+            indices_new[i] = indices_new[i-1] + 1
+        end
+    end
+    # get a new site list
+    site_list = SL[site_list[i] for i in 1:length(site_list) if !(i in index)]
+    # register new list of sites within the lattice object
+    sites!(lattice, site_list)
+
+
+    # get all bonds that are not connected to any removed site
+    bond_list = filter(b -> !(to(b) in index || from(b) in index), bonds(lattice))
+    # relabeling of bond indices
+    for b in bond_list
+        # set the new indices
+        from!(b, indices_new[from(b)])
+        to!(b,   indices_new[to(b)])
+    end
+    # set the bond list in the lattice object
+    bonds!(lattice, bond_list)
+
+
+    # return the removed sites
+    return sites_to_return
+end
