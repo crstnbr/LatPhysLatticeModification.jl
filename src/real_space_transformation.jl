f2cb503b3c8a7bd6889bd5f36ecdaf8d2a6e4696
Jan Attig <jattig@thp.Uni-Koeln.DE>
2019-01-15 11:30:18 +0100

    added z coordinate to be chosable when embedding in 3D

--- src/real_space_transformation.jl
+++ src/real_space_transformation.jl
@@ -1,10 +1,11 @@
 function getEmbeddedIn3D(
-            unitcell :: U
+            unitcell :: U,
+            z :: Real = 0.0
         ) :: Unitcell{Site{LS,3}, B} where {LS,B,S<:AbstractSite{LS,2},U<:AbstractUnitcell{S,B}}
 
     # build a new list of sites
     sites_new = Site{LS,3}[
-        newSite(Site{LS,3}, [point(s)[1], point(s)[2], 0.0], label(s)) for s in sites(unitcell)
+        newSite(Site{LS,3}, [point(s)[1], point(s)[2], z], label(s)) for s in sites(unitcell)
     ]
 
     # create and return a new Unitcell
@@ -17,12 +18,13 @@ function getEmbeddedIn3D(
 end
 
 function getEmbeddedIn3D(
-            lattice :: L
+            lattice :: L,
+            z :: Real = 0.0
         ) :: Lattice{Site{LS,3}, B, U} where {LS,B,S<:AbstractSite{LS,2},U,L<:AbstractLattice{S,B,U}}
 
     # build a new list of sites
     sites_new = Site{LS,3}[
-        newSite(Site{LS,3}, [point(s)[1], point(s)[2], 0.0], label(s)) for s in sites(lattice)
+        newSite(Site{LS,3}, [point(s)[1], point(s)[2], z], label(s)) for s in sites(lattice)
     ]
 
     # create and return a new Unitcell
